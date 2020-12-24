557b47329df1cac81ef8fe4f725ae677d05424e4
Omar I. Martínez Gómez <omar_238@hotmail.com>
2020-12-24 00:06:13 -0600

    pp 7-31

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Chapter1
 {
     public static class Program
     {
-
-
-        public static void Main(string[] args)
+        public static void ThreadMethod(object o)
         {
+            for (int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void Main()
+        {
+            bool stopped = false;
+            Thread t = new Thread(new ThreadStart(() =>
+            {
+                while (!stopped)
+                {
+                    Console.WriteLine("Running...");
+                    Thread.Sleep(1000);
+                }
+            }));
+            
+            t.Start();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            stopped = true;
+            t.Join();
         }
     }
 }
